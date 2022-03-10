91ced19389d6578c0faf8d824dae4960e7c5ddc3
unknown <aik0124k@gmail.com>
2022-03-10 13:02:38 +0100

    images server handler

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-import json
+from PIL import Image
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+images_previus = []
+images_next = []
 
 
 @app.route("/", methods=['GET', "POST"])
@@ -11,23 +14,65 @@ def index():
 
 @app.route("/send_last_job", methods=['POST',])
 def get_data():
-
-    input_json = request.get_json(force=True)
-    print('data from client:', input_json)
-
     results = {'processed': 'true'}
+
+    receive_data = request.get_json(force=True)
+    print('receive data from client:', receive_data)
+
+    image = receive_data.get("image", None)
+
+    if image != None:
+        images_previus.append(image)
+
     return jsonify(results)
 
 
 @app.route("/get_last_job", methods=['POST',])
 def send_data():
-    input_json = request.get_json(force=True)
-    count = input_json.get("count", 0)
+    results = {'processed': 'true'}
+    results += {'retuned': 0}
+
+    receive_data = request.get_json(force=True)
+    count = receive_data.get("count", 0)
 
     if count == 0:
-        return
+        pass
+    
+    if count >= len(images_previus):
+        images_return = images_previus
+        get_undos(images_return)
+        
+        results += {'images_previus': images_return}
+        results['returned'] = len(images_return)
 
+    if count < len(images_previus):
+        images_return = images_previus[:count]
+        get_undos(images_return)
+        
+        results += {'images_previus': images_return}
+        results['returned'] = len(images_return)
+
+    return jsonify(results)
+
+
+def get_undos(images_returned):
+    images_previus -= images_returned
+    images_next += images_returned
+
+@app.route("/save", methods=['GET','POST'])
+def save_job():
     results = {'processed': 'true'}
+    results += {'saved': 'false'}
+
+    for index in range(len(images_previus)):
+        try:
+            img = Image.open(images_previus[index])
+            img.save(f"./database/{index}.jpg")
+
+            results["saved"] = 'true'
+        except:
+            pass
+
     return jsonify(results)
 
 
