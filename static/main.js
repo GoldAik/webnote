0af9777a80abd33574ed9425dc702d87dd0948b7
unknown <aik0124k@gmail.com>
2022-03-10 13:27:56 +0100

    application base

--- static/main.js
+++ static/main.js
@@ -1,28 +1,62 @@
+const canvasParent = document.getElementById("main")
 const canvas = document.getElementById("canvas")
 let context = canvas.getContext("2d")
 
-let color = "red"
+let backgroundColor = "white"
+let color = "white"
 let size = 10
 
 let isTouching = false
-
 let undos = new Array()
 
-canvas.addEventListener("touchstart", () => {isTouching = true})
-canvas.addEventListener("touchend", () => {isTouching = false})
 
-window.addEventListener('load', () => {
-    window.addEventListener('resize', resizeCanvas, false)
-})
+const beforeDraw = () => {
+    console.log(`Begining draw`)
+    isTouching = true
+}
 
 
 const draw = () => {
-    console.log("draw" + color + str(size))
+    if(!isTouching) return
+    console.log(`Drawing with color: ${color} and size: ${size}`)
 }
+
+
+const stopDraw = () => {
+    console.log(`Drawing was stoped`)
+    isTouching = false
+}
+
+const getWidth = () => canvasParent.offsetWidth
+const getHeight = () => canvasParent.offsetHeight
 
 const resizeCanvas = () => {
-    console.log(`resize ${canvas.hight} | ${canvas.width}`)
 
-    // canvas.height = 0
-    // canvas.width = 0
+    canvas.width = getWidth()
+    canvas.height = getHeight()
+
+    context.fillStyle = color
+    context.fillRect(0, 0, getWidth(), getHeight())
+
+    console.log(`resize ${canvas.width} | ${canvas.height}`)
+
 }
+
+const init = () =>{
+    canvas.addEventListener("touchstart", beforeDraw, false)
+    canvas.addEventListener("touchmove", draw, false)
+    canvas.addEventListener("touchend", stopDraw, false)
+
+    canvas.addEventListener("mousedown", beforeDraw, false)
+    canvas.addEventListener("mousemove", draw, false)
+    canvas.addEventListener("mouseup", stopDraw, false)
+
+    resizeCanvas()
+
+    window.addEventListener('resize', resizeCanvas, false)
+}
+
+
+window.addEventListener('load', () => {  // this one or make init from html
+     init()
+})
