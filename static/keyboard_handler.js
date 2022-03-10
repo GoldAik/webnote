9cdeb85c1cdcbdf91ebd1ce1e59df416e818028f
unknown <aik0124k@gmail.com>
2022-03-10 15:46:40 +0100

    basic functionality with some bugs

---
+++ static/keyboard_handler.js
@@ -0,0 +1,35 @@
+let pressedKeys = new Array()
+
+const shortcutHandler = () =>{
+    console.log(pressedKeys)
+    if(pressedKeys.includes("Control") &&  pressedKeys.includes("z")){
+        undo()
+    }
+    else if(pressedKeys.includes("Control") &&  pressedKeys.includes("Shift") && pressedKeys.includes("Z")){
+        next()
+    }
+    else if(pressedKeys.includes("Control") &&  pressedKeys.includes("Delete")){
+        clearCanvas()
+    }
+}
+
+
+const pressKey = (e) =>{
+    let key = e.key
+
+    if(pressedKeys.includes(key)) return
+    
+    pressedKeys.push(key)
+    shortcutHandler()
+}
+
+
+const releaseKey = (e) =>{
+    let key = e.key
+    
+    pressedKeys.pop(key)
+}
+
+
+document.addEventListener('keydown', pressKey, false)
+document.addEventListener('keyup', releaseKey, false)
--- static/main.js
+++ static/main.js
@@ -3,55 +3,120 @@ const canvas = document.getElementById("canvas")
 let context = canvas.getContext("2d")
 
 let backgroundColor = "white"
-let color = "white"
+let color = "black"
 let size = 10
 
 let isTouching = false
-let undos = new Array()
 
+let previusStorage = new Array()
+let nextStorage = new Array()
 
-const beforeDraw = () => {
-    console.log(`Begining draw`)
-    isTouching = true
+let storage = {
+    previusStorage: new Array(),
+    nextStorage: new Array()
 }
 
 
-const draw = () => {
+const getPointX = (e) => e.clientX - canvas.offsetLeft
+const getPointY = (e) => e.clientY - canvas.offsetTop
+
+const beforeDraw = (e) => {
+    console.log(`Begining draw`)
+    isTouching = true
+
+    context.beginPath()
+    context.moveTo(getPointX(e), getPointY(e))
+    e.preventDefault()
+}
+
+
+const draw = (e) => {
     if(!isTouching) return
     console.log(`Drawing with color: ${color} and size: ${size}`)
+
+    context.lineTo(getPointX(e), getPointY(e))
+    context.strokeStyle = color
+    context.lineWidth = size
+    context.lineCap = "round"
+    context.lineJoin = "round"
+    context.stroke()
+
+    e.preventDefault()
 }
 
 
 const stopDraw = () => {
     console.log(`Drawing was stoped`)
+    if(!isTouching) return
+
+    context.closePath()
     isTouching = false
+
+    saveToStorage()
 }
+
+
+const undo = () => {
+    if(previusStorage.length == 0) return
+
+    presentCanvasImage = context.getImageData(0, 0, canvas.width, canvas.height)
+    nextStorage.push(presentCanvasImage)
+
+    lastCanvasImage = previusStorage.pop()
+    context.putImageData(lastCanvasImage, 0, 0)
+}
+
+
+const next = () => {
+    if(nextStorage.length == 0) return
+
+    presentCanvasImage = context.getImageData(0, 0, canvas.width, canvas.height)
+    previusStorage.push(presentCanvasImage)
+
+    lastCanvasImage = nextStorage.pop()
+    context.putImageData(lastCanvasImage, 0, 0)
+}
+
+
+const saveToStorage = () => {
+    imageCanvas = context.getImageData(0, 0, canvas.width, canvas.height)
+    previusStorage.push(imageCanvas)
+    nextStorage = []
+}
+
+
+const clearCanvas = () =>{
+    saveToStorage()
+
+    context.fillStyle = backgroundColor
+    context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
 
 const getWidth = () => canvasParent.offsetWidth
 const getHeight = () => canvasParent.offsetHeight
 
 const resizeCanvas = () => {
-
     canvas.width = getWidth()
     canvas.height = getHeight()
-
-    context.fillStyle = color
-    context.fillRect(0, 0, getWidth(), getHeight())
 
     console.log(`resize ${canvas.width} | ${canvas.height}`)
 
 }
 
+
 const init = () =>{
     canvas.addEventListener("touchstart", beforeDraw, false)
     canvas.addEventListener("touchmove", draw, false)
-    canvas.addEventListener("touchend", stopDraw, false)
+    window.addEventListener("touchend", stopDraw, false)
 
     canvas.addEventListener("mousedown", beforeDraw, false)
     canvas.addEventListener("mousemove", draw, false)
-    canvas.addEventListener("mouseup", stopDraw, false)
+    window.addEventListener("mouseup", stopDraw, false)
+    canvas.addEventListener("mouseout", stopDraw, false)
 
     resizeCanvas()
+    clearCanvas()
 
     window.addEventListener('resize', resizeCanvas, false)
 }
